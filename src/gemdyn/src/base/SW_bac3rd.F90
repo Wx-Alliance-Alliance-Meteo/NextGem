0864c73f70c02cb13ee1a5ae486e0a91270ac02c
Jean-Francois Cossette <jean-francois.cossette@ec.gc.ca>
2024-10-22 17:28:02 +0000

    new modifs

--- src/gemdyn/src/base/SW_bac3rd.F90
+++ src/gemdyn/src/base/SW_bac3rd.F90
@@ -78,6 +78,9 @@
       if ( Grd_yinyang_L) then
          call yyg_xchng_8 (F_q(l_minx,l_miny,1), YYG_HALO_q2q, l_minx,l_maxx,l_miny,l_maxy, &
                            l_ni,l_nj, l_nk, .false., 'CUBIC', .true.)
+!        call HLT_split (1, G_nk, HLT_np, HLT_start, HLT_end)
+!        call gem_xch_halo_8 ( F_q(l_minx,l_miny,HLT_start),&
+!                   l_minx,l_maxx,l_miny,l_maxy, HLT_np,-1)
       else
         !call HLT_split (F_k0, F_kn, HLT_np, HLT_start, HLT_end)
          call HLT_split (1, G_nk, HLT_np, HLT_start, HLT_end)
@@ -86,11 +89,14 @@
       endif
 
 !!$omp do collapse(2)
-      do k=k0, l_nk
-         do j= j0, jn
-            km=max(k-1,1)
+!     do k=k0, l_nk
+!        do j= j0, jn
+!           km=max(k-1,1)
 !DIR$ SIMD
-            do i= i0, l_niu-pil_e
+!           do i= i0, l_niu-pil_e
+      do k=ds_k0, l_nk
+         do j= ds_j0, ds_jn
+            do i= ds_i0, l_niu-pil_e
 
                dqdx = Hderiv8(F_q(i-1,j,k), F_q(i,j,k), &
                              F_q(i+1,j,k), F_q(i+2,j,k), geomh_invDX_8(j))
@@ -104,10 +110,11 @@
       
 !!$omp do collapse(2)
       do k=k0, l_nk
-         do j= j0, l_njv-pil_n
-            km=max(k-1,1)
+!        do j= j0, l_njv-pil_n
 !DIR$ SIMD
-            do i= i0, in
+!           do i= i0, in
+         do j= ds_j0, l_njv-pil_n
+            do i= ds_i0, ds_in
 
                dqdy = Hderiv8(F_q(i,j-1,k),F_q(i  ,j,k),&
                              F_q(i,j+1,k),F_q(i,j+2,k),geomh_invDY_8)
